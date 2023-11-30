4c6b87cf65c1a25e167cb3a879dc8dcca6e58d51
kakuilan <kakuilan@163.com>
2023-12-01 00:24:51 +0800

    fix Declaration of JsonRpc\Exception\RequestException::create

--- src/Exception/RequestException.php
+++ src/Exception/RequestException.php
@@ -18,37 +18,40 @@ use Exception;
 use Graze\GuzzleHttp\JsonRpc\Message\RequestInterface;
 use Graze\GuzzleHttp\JsonRpc\Message\ResponseInterface;
 use GuzzleHttp\Exception\RequestException as HttpRequestException;
+use GuzzleHttp\BodySummarizerInterface;
 use Psr\Http\Message\RequestInterface as HttpRequestInterface;
 use Psr\Http\Message\ResponseInterface as HttpResponseInterface;
 
-class RequestException extends HttpRequestException
-{
+class RequestException extends HttpRequestException {
+
     /**
      * {@inheritdoc}
      *
-     * @param HttpRequestInterface       $request        Request
-     * @param HttpResponseInterface|null $response       Response received
-     * @param \Exception|null            $previous       Previous exception
-     * @param array|null                 $handlerContext Optional handler context.
+     * @param HttpRequestInterface $request Request
+     * @param HttpResponseInterface|null $response Response received
+     * @param \Throwable|null $previous Previous exception
+     * @param array|null $handlerContext Optional handler context
+     * @param BodySummarizerInterface|null $bodySummarizer Optional body summarizer
      *
      * @return HttpRequestException
      */
     public static function create(
-        HttpRequestInterface $request,
-        HttpResponseInterface $response = null,
-        Exception $previous = null,
-        array $handlerContext = null
-    ) {
+        HttpRequestInterface    $request,
+        HttpResponseInterface   $response = null,
+        \Throwable              $previous = null,
+        array                   $handlerContext = [],
+        BodySummarizerInterface $bodySummarizer = null
+    ): parent {
         if ($request instanceof RequestInterface && $response instanceof ResponseInterface) {
             static $clientErrorCodes = [-32600, -32601, -32602, -32700];
 
             $errorCode = $response->getRpcErrorCode();
             if (in_array($errorCode, $clientErrorCodes)) {
                 $label = 'Client RPC error response';
-                $className = ClientException::class;
+                //$className = ClientException::class;
             } else {
                 $label = 'Server RPC error response';
-                $className = ServerException::class;
+                //$className = ServerException::class;
             }
 
             $message = $label . ' [uri] ' . $request->getRequestTarget()
@@ -56,9 +59,9 @@ class RequestException extends HttpRequestException
                 . ' [error code] ' . $errorCode
                 . ' [error message] ' . $response->getRpcErrorMessage();
 
-            return new $className($message, $request, $response, $previous);
+            return new parent($message, $request, $response, $previous, $handlerContext);
         }
 
-        return parent::create($request, $response, $previous);
+        return parent::create($request, $response, $previous, $handlerContext);
     }
 }
